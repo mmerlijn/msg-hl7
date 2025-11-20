5e7069b215da7ed69313dc29dcac92267c31fbbd
Menno <menno@mmerlijn.nl>
2025-11-20 13:12:24 +0100

    fix orc reader

--- src/segments/ORC.php
+++ src/segments/ORC.php
@@ -8,6 +8,7 @@ use mmerlijn\msgRepo\Address;
 use mmerlijn\msgRepo\Enums\OrderControlEnum;
 use mmerlijn\msgRepo\Msg;
 use mmerlijn\msgRepo\Name;
+use mmerlijn\msgRepo\Organisation;
 use mmerlijn\msgRepo\Request;
 
 class ORC extends Segment implements SegmentInterface
@@ -50,9 +51,26 @@ class ORC extends Segment implements SegmentInterface
         $msg->order->entered_by->agbcode = $this->getData(10);
         $msg->order->entered_by->source = $this->getData(10, 0, 8);
 
-        $msg->order->organisation->name = $this->getData(17, 0, 1);
-        $msg->order->organisation->agbcode = $this->getData(17);
-        $msg->order->organisation->source = $this->getData(17, 0, 2);
+        if ($this->getData(21, 0, 2)) {
+            $organisation = new Organisation(
+                name: $this->getData(21),
+                agbcode: $this->getData(21, 0, 2),
+                source: $this->getData(21, 0, 5),
+            );
+        } elseif ($this->getData(17)) {
+            $organisation = new Organisation(
+                name: $this->getData(17, 0, 1),
+                agbcode: $this->getData(17),
+                source: $this->getData(17, 0, 2),
+            );
+        } elseif ($this->getData(13, 0, 3, 1)) {
+            $organisation = new Organisation(
+                name: $this->getData(13, 0, 3),
+                agbcode: $this->getData(13, 0, 3, 1),
+            );
+        }
+        $msg->order->organisation = $organisation ?? new Organisation();
+
         if (str_starts_with($this->getData(12), "0")) {
             $msg->patient->last_requester = $this->getData(12);
         } elseif (str_starts_with($this->getData(17), "0")) {
@@ -82,10 +100,12 @@ class ORC extends Segment implements SegmentInterface
 
         return $msg;
     }
+
     public function setMsg($msg): self
     {
         return $this->setOrder($msg);
     }
+
     public function setOrder($msg): self
     {
         //order controle
@@ -111,8 +131,12 @@ class ORC extends Segment implements SegmentInterface
         $this->setData($msg->order->requester->name->getLastnames(), 12, 0, 1);
         $this->setData($msg->order->requester->name->initials, 12, 0, 2);
         $this->setData($msg->order->requester->source, 12, 0, 8);
-        $this->setData($msg->order->requester->location, 13,0,8);
+        $this->setData($msg->order->requester->location, 13, 0, 8);
 
+        //organisation
+        $this->setData($msg->order->organisation->agbcode, 13, 0, 3, 1);
+        $this->setData($msg->order->organisation->name, 13, 0, 3);
+        $this->setData($msg->order->organisation->name, 13, 0, 8);
 
         //organization
         $this->setData($msg->order->organisation->agbcode, 17);
@@ -134,10 +158,10 @@ class ORC extends Segment implements SegmentInterface
         $this->setData($msg->sender->address->country ?: "NL", 22, 0, 5);
 
         //set telephone
-        if($msg->sender->phone){
+        if ($msg->sender->phone) {
             $this->setData($msg->sender->phone, 23);
-            $this->setData("WPN",23,0,1);
-            $this->setData("PH",23,0,2);
+            $this->setData("WPN", 23, 0, 1);
+            $this->setData("PH", 23, 0, 2);
         }
 
         return $this;
--- tests/Unit/segments/Orcv3Test.php
+++ tests/Unit/segments/Orcv3Test.php
@@ -1,7 +1,10 @@
 <?php
 
 use mmerlijn\msgHl7\Hl7;
+use mmerlijn\msgRepo\Contact;
 use mmerlijn\msgRepo\Msg;
+use mmerlijn\msgRepo\Name;
+use mmerlijn\msgRepo\Organisation;
 
 it('can read ORC', function (string $hl7, Msg $expectedRepo) {
     $hl7 = new Hl7($hl7);
@@ -25,8 +28,7 @@ it('can read ORC', function (string $hl7, Msg $expectedRepo) {
         ->and($msg->sender->address->city)->toBe($expectedRepo->sender->address->city)
         ->and($msg->sender->address->postcode)->toBe($expectedRepo->sender->address->postcode)
         ->and($msg->sender->address->country)->toBe($expectedRepo->sender->address->country)
-        ->and($msg->sender->phone->number)->toBe($expectedRepo->sender->phone->number)
-        ;
+        ->and($msg->sender->phone->number)->toBe($expectedRepo->sender->phone->number);
 })->with([
     ["MSH|^~\&|ZorgDomein||Labtrain|SALT|20251112125133+0100||OML^O21^OML_O21|d0a06274854e4824a8a4|P|2.5.1|||||NLD|8859/1
 ORC|NW|ZP100120392||ZP100120392|||^^^^^R||20251112125346+0100|01123456^Blank^M.A.^^^^^^VEKTIS||01123456^Blank^M.A.^^^^^^VEKTIS|^^^^^^^^SALT||||50009046^SALT^VEKTIS||||SALT^^50009046^^^VEKTIS|Molenwerf 11&Molenwerf&11^^Koog aan de Zaan^^1541WR^NL|0889100100^WPN^PH
@@ -53,7 +55,7 @@ ORC|NW|ZP100120392||ZP100120392|||^^^^^R||20251112125346+0100|01123456^Blank^M.A
                     ),
                     source: "VEKTIS"
                 ),
-                organisation: new \mmerlijn\msgRepo\Organisation(
+                organisation: new Organisation(
                     name: "SALT",
                     agbcode: "50009046",
                     source: "VEKTIS"
@@ -68,10 +70,9 @@ ORC|NW|ZP100120392||ZP100120392|||^^^^^R||20251112125346+0100|01123456^Blank^M.A
                     building_nr: "11",
                     country: "NL"
                 ),
-                phone:
-                    new \mmerlijn\msgRepo\Phone(
-                        number: "0889100100"
-                    )
+                phone: new \mmerlijn\msgRepo\Phone(
+                    number: "0889100100"
+                )
 
             )
         ),
@@ -79,7 +80,6 @@ ORC|NW|ZP100120392||ZP100120392|||^^^^^R||20251112125346+0100|01123456^Blank^M.A
     ],
 ]);
 it('can write ORC', function (\mmerlijn\msgRepo\Msg $msg, string $expectedPid) {
-
 
     $orc = new \mmerlijn\msgHl7\segments\ORC();
     $orc->setMsg($msg);
@@ -110,7 +110,7 @@ it('can write ORC', function (\mmerlijn\msgRepo\Msg $msg, string $expectedPid) {
                     ),
                     source: "VEKTIS"
                 ),
-                organisation: new \mmerlijn\msgRepo\Organisation(
+                organisation: new Organisation(
                     name: "SALT",
                     agbcode: "50009046",
                     source: "VEKTIS"
@@ -125,8 +125,7 @@ it('can write ORC', function (\mmerlijn\msgRepo\Msg $msg, string $expectedPid) {
                     building_nr: "11",
                     country: "NL"
                 ),
-                phone:
-                new \mmerlijn\msgRepo\Phone(
+                phone: new \mmerlijn\msgRepo\Phone(
                     number: "0889100100"
                 )
 
@@ -134,6 +133,60 @@ it('can write ORC', function (\mmerlijn\msgRepo\Msg $msg, string $expectedPid) {
         ),
         "ORC|NW|ZP100120392||ZP100120392|||^^^^^R||20251112125346+0100|01123456^Blank^MA^^^^^^VEKTIS||01123456^Blank^MA^^^^^^VEKTIS|^^^^^^^^SALT||||50009046^SALT^VEKTIS||||SALT^^50009046^^^VEKTIS|Molenwerf 11&Molenwerf&11^^Koog Aan De Zaan^^1541WR^NL|088 9100 100^WPN^PH"
     ],
+]);
+
+it('can read requester and organisation', function (string $orc, Contact $requester, Organisation $organisation) {
+    $hl7 = new Hl7("MSH|^~\&|ZorgDomein||Labtrain|SALT|20251112125133+0100||OML^O21^OML_O21|d0a06274854e4824a8a4|P|2.5.1|||||NLD|8859/1
+" . $orc);
+    $msg = $hl7->getMsg(new Msg());
+    expect($msg->order->organisation->name)->toBe($organisation->name)
+        ->and($msg->order->organisation->agbcode)->toBe($organisation->agbcode)
+        ->and($msg->order->organisation->source)->toBe($organisation->source)
+        ->and($msg->order->requester->name)->toMatchObject($requester->name)
+        ->and($msg->order->requester->agbcode)->toBe($requester->agbcode);
+})->with([
+    ["ORC|NW|ZP12345||ZP12345|||^^^^^R||20251212113429+0100|^Joe^s||01234567^Doe^J^^^^^^VEKTIS|^^^HP Doe&07654321^^^^^HP Doe||||07654321^HP Doe^VEKTIS||||HP Doe^^07654321^^^VEKTIS",
+        fn() => new Contact(
+            agbcode: "01234567",
+            name: new Name(
+                initials: "J",
+                name: "Doe"
+            ),
+        ),
+        fn() => new Organisation(
+            name: "HP Doe",
+            agbcode: "07654321",
+            source: "VEKTIS"
+        )
+    ], [
+        "ORC|NW|ZP12345||ZP12345|||^^^^^R||202511201040|_background||01234567^Doe, J.^J.^^^^^^vektis",
+        fn() => new Contact(
+            agbcode: "01234567",
+            name: new Name(
+                initials: "J",
+                name: "Doe"
+            ),
+        ),
+        fn() => new Organisation(
+            name: "",
+            agbcode: null,
+            source: null
+        )
+    ], [
+        "ORC|NW|ZP12345||ZP12345|||^^^^^R||20251212113429+0100|^Joe^s||01122339^Str^H.J.^^^^^^VEKTIS|^^^HP Str&09332211^^^^^HP Str||||09332211^HP Str^VEKTIS||||HP Str^^09332211^^^VEKTIS",
+        fn() => new Contact(
+            agbcode: "01122339",
+            name: new Name(
+                initials: "H.J.",
+                name: "Str"
+            ),
+        ),
+        fn() => new Organisation(
+            name: "HP Str",
+            agbcode: "09332211",
+            source: "VEKTIS"
+        )
+    ]
 ]);
 
 
