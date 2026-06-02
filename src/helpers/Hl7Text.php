7634105e98ac80f0bf969e3366f0e9ea1dbf1b7d
Menno <menno@mmerlijn.nl>
2026-06-02 23:24:37 +0200

    fix

--- src/helpers/Hl7Text.php
+++ src/helpers/Hl7Text.php
@@ -5,63 +5,46 @@ namespace mmerlijn\msgHl7\helpers;
 class Hl7Text
 {
     public mixed $data;
-    public function __construct(mixed $data){
-        $this->data = $data??'';
+
+    public function __construct(mixed $data)
+    {
+        $this->data = $data ?? '';
         return $this;
     }
-    public function encode():string
-    {
-        $this->encodeLineBreaks();
-        $this->encodeSpecials();
-        return trim($this->data);
-    }
-    public function decode():string
-    {
-        $this->decodeLineBreaks();
-        $this->decodeSpecials();
-        return trim($this->data);
-    }
 
-    public function encodeLineBreaks():void
+    public function encode(): string
     {
-        //alle line breaks omzetten naar \.br\ <=>PHP_EOL
-        if (is_array($this->data)) {
-            $data = array_map(function ($item) {
-                return self::encode($item);
-            }, $this->data);
-            $this->data = implode("\\.br\\", $this->data);
+        if (!is_array($this->data)) {
+            $this->data = [$this->data];
         }
-        $this->data = preg_replace('/\r\n|\r|\n/', '\\.br\\', $this->data);
+        $this->data = array_map(function ($item) {
+            $item = preg_replace('/\\\/', '\\E\\', $item); //backslash
+            //$item = preg_replace('/\\\r/', '', $item); //carriage return
+            $item = preg_replace('/\|/', '\\F\\', $item); //field separator |
+            $item = preg_replace('/~/', '\\R\\', $item); //repetition separator ~
+            $item = preg_replace('/\^/', '\\S\\', $item); //component separator ^
+            $item = preg_replace('/&/', '\\T\\', $item); //subcomponent separator &
+            return preg_replace('/\r\n|\r|\n/', '\\.br\\', $item);
+        }, $this->data);
+        $this->data = implode("\\.br\\", $this->data);
+
+
+        return trim($this->data);
     }
 
-    public function encodeSpecials():void
+    public function decode(): string
     {
-
-        $this->data = preg_replace('/\\\/', '\\E\\', $this->data); //backslash
-        //$this->data = preg_replace('/\\\r/', '', $this->data); //carriage return
-        $this->data = preg_replace('/\|/', '\\F\\', $this->data); //field separator |
-        $this->data = preg_replace('/~/', '\\R\\', $this->data); //repetition separator ~
-        $this->data = preg_replace('/\^/', '\\S\\', $this->data); //component separator ^
-        $this->data = preg_replace('/&/', '\\T\\', $this->data); //subcomponent separator &
-
-    }
-
-
-    public function decodeLineBreaks():void
-    {
-        $this->data = str_replace('\\.br\\', PHP_EOL, $this->data);
-    }
-
-    public function decodeSpecials():void
-    {
-        $this->data = preg_replace( '/\\\E\\\/','/\\\/', $this->data); //backslash
+        $this->data = preg_replace('/\\\E\\\/', '/\\\/', $this->data); //backslash
         //$this->data = preg_replace( '/\\\r/','', $this->data); //carriage return
-        $this->data = preg_replace( '/\\\F\\\/','|', $this->data); //field separator |
-        $this->data = preg_replace( '/\\\R\\\/','~', $this->data); //repetition separator ~
-        $this->data = preg_replace( '/\\\S\\\/','^', $this->data); //component separator ^
-        $this->data = preg_replace( '/\\\T\\\/','&', $this->data); //subcomponent separator &
+        $this->data = preg_replace('/\\\F\\\/', '|', $this->data); //field separator |
+        $this->data = preg_replace('/\\\R\\\/', '~', $this->data); //repetition separator ~
+        $this->data = preg_replace('/\\\S\\\/', '^', $this->data); //component separator ^
+        $this->data = preg_replace('/\\\T\\\/', '&', $this->data); //subcomponent separator &
         $this->data = preg_replace('/\\\.*\\\/', '', $this->data); //diverse tekens
         $this->data = preg_replace('/\s+/', ' ', $this->data); //multiple spaces to 1
-
+        $this->data = str_replace('\\.br\\', PHP_EOL, $this->data);
+        return trim($this->data);
     }
+
+
 }
